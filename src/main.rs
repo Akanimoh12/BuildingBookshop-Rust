9bd04e0250cdef60efeaa0e8f9cd473d003af99f
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-05-29 11:06:14 +0100

    Building Bookshop using Rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::string;
+use std::{fmt::format, string};
 
+#[derive(Clone, Copy, Debug)]
  enum Status {
         Available,
         Checked,
@@ -23,12 +24,34 @@ use std::string;
 
     impl Book {
 
-        fn constructor(title:&str, author:String, pages:u64, status:Status, genre:Genre )->Book {
-            Book { title:title.to_string(), author, pages, status, genre }
+        fn new(title:&str, author:String, pages:u64, status:Status, genre:Genre )->Self {
+            Self{ title:title.to_string(), author, pages, status, genre }
         }
 
-        fn checked(){
+        fn checked(&mut self){
+            self.status=Status::Checked
+        }
 
+        fn book(&self)->&Self{
+            self
+        }
+
+        fn underRepair(&mut self){
+            self.status=Status::UnderRepair
+        }
+
+        fn bookInfo(&self)->String{
+            let title = self.title.clone();
+            let author=self.author.clone();
+            let pages = self.pages.clone();
+            let status=self.status;
+            let genre=self.genre;
+            
+            let fnt=format!("hello the book name is {} by {} it has {} pages", )
+        }
+
+        fn can_be_borrowed(&self){
+            if
         }
 
     }
